7cd35309d255ac243c2e40ac2bca9ec33a1ad941
RAMON ARAUJO OLIVEIRA <ramonaololiveira@gmail.com>
2025-06-09 18:05:50 -0300

    Vesion: 009.000.001.000

--- src/Application.php
+++ src/Application.php
@@ -162,7 +162,7 @@ class Application extends Container
      */
     public function version()
     {
-        return 'Lumen (9.0.0) (Laravel Components ^9.0)';
+        return 'Lumen (9.0.1) (Laravel Components ^9.0)';
     }
 
     /**
@@ -829,6 +829,17 @@ class Application extends Container
     public function databasePath($path = '')
     {
         return $this->basePath.DIRECTORY_SEPARATOR.'database'.($path ? DIRECTORY_SEPARATOR.$path : $path);
+    }
+
+    /**
+     * Get the path to the language files.
+     *
+     * @param  string  $path
+     * @return string
+     */
+    public function langPath($path = '')
+    {
+        return $this->getLanguagePath().($path != '' ? DIRECTORY_SEPARATOR.$path : '');
     }
 
     /**
